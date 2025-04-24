178ff65493597d38a4b9e819d1d1611a79e1cd53
Colin Wilson <furious.gecko@gmail.com>
2025-04-24 16:01:57 +0100

    Got 05_summAll tests passing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    // check for negative numbers
+    if(start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+    // check for non-integers
+    if(start % 1 != 0 || end % 1 != 0) {
+        return "ERROR";
+    }
+
+    // check for non numbers
+    if(!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+
+    let first = Math.min(start, end);
+    let last = Math.max(start, end);
+
+    let sum = 0;
+
+    for(let i = first; i <= last; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
